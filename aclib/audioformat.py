d10b71677b8be439ef3a6bdda25aa15f4ce2142f
Matt Spitz <mattspitz@gmail.com>
2017-01-16 01:00:03 -0500

    Adding read tags

--- aclib/audioformat.py
+++ aclib/audioformat.py
@@ -1,3 +1,6 @@
+import re
+import subprocess
+
 from recordclass import recordclass
 
 Tags = recordclass(
@@ -11,7 +14,8 @@ Tags = recordclass(
         "track_no",
         "cd_no",
         "cd_tracks",
-        "genre",
+        "genre_id",
+        "genre_name",
         "comment",
         "composer",
         "original_artist",
@@ -53,4 +57,65 @@ class MP3(BaseFormat):
 
     @classmethod
     def read_tags(cls, fn):
-        pass
+        output = subprocess.check_output([
+            "id3v2",
+            "--list-rfc822",
+            fn,
+        ])
+
+        tags = {}
+        for line in output.strip().split("\n"):
+            key, val = line.split(": ", 1)
+            tags[key] = val
+
+        if "TYER" in tags:
+            try:
+                year = int(tags["TYER"])
+            except ValueError:
+                year = tags["TYER"]
+        else:
+            year = None
+
+        if "TRCK" in tags:
+            trck = tags["TRCK"]
+            if "/" in trck:
+                track_no, cd_tracks = map(int, trck.split("/"))
+            else:
+                track_no = int(trck)
+        else:
+            track_no, cd_tracks = None, None
+
+        if "TPOS" in tags:
+            tpos = int(tags["TPOS"])
+        else:
+            tpos = None
+
+        if "TCON" in tags:
+            match = re.match(r"(?P<genre_name>.*) \((?P<genre_id>\d+)\)", tags["TCON"])
+            genre_name = match.group("genre_name")
+            genre_id = int(match.group("genre_id"))
+        else:
+            genre_name, genre_id = None, None
+
+        if "COMM" in tags:
+            # format is like '()[]: Comment'
+            comment = tags["COMM"].split(": ", 1)[1]
+        else:
+            comment = None
+
+        return Tags(
+            album_artist=tags.get("TPE2"),
+            artist=tags.get("TPE1"),
+            title=tags.get("TIT2"),
+            album=tags.get("TALB"),
+            year=year,
+            track_no=track_no,
+            cd_no=tpos,
+            cd_tracks=cd_tracks,
+            genre_id=genre_id,
+            genre_name=genre_name,
+            comment=comment,
+            composer=tags.get("TCOM"),
+            original_artist=tags.get("TOPE"),
+            encoded_by=tags.get("TENC"),
+        )
--- aclib/tests/audioformat_test.py
+++ aclib/tests/audioformat_test.py
@@ -1,11 +1,14 @@
 import os
 
-from ..audioformat import MP3
+from ..audioformat import (
+    MP3,
+    Tags,
+)
 
 # http://doc.pytest.org/en/latest/tmpdir.html ?
 
 FIXTURES_DIR = os.path.join(
-    os.path.dirname(__name__),
+    os.path.dirname(__file__),
     "fixtures",
 )
 
@@ -13,4 +16,23 @@ WAV_FN = os.path.join(FIXTURES_DIR, "makeitso.wav")
 MP3_FN = os.path.join(FIXTURES_DIR, "makeitso.mp3")
 
 def test_mp3_read_tags():
-    MP3.read_tags(MP3_FN)
+    tags = MP3.read_tags(MP3_FN)
+
+    expected_tags = Tags(
+        album_artist="Star Trek",
+        artist="Jean-Luc Picard",
+        title="Make It So",
+        album="Sample Tracks",
+        year=2016,
+        track_no=4,
+        cd_no=3,
+        cd_tracks=22,
+        genre_id=132,
+        genre_name="Britpop",
+        comment="Comment!",
+        composer="Composer!",
+        original_artist="Original Artist!",
+        encoded_by="Encoded By!",
+    )
+
+    assert tags == expected_tags
