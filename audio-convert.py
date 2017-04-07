91352dc7d997830333a8a29b4a6ceb34751da108
Matt Spitz <mattspitz@gmail.com>
2017-04-07 01:08:33 -0400

    Improvements.

--- audio-convert.py
+++ audio-convert.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
 
 import argparse
+import errno
 import itertools
 import operator
 import os
@@ -18,6 +19,16 @@ from aclib import (
     util,
 )
 
+PROCESSED_DIR = "processed"
+
+# courtesy of http://stackoverflow.com/questions/2890146/how-to-force-pyyaml-to-load-strings-as-unicode-objects
+def construct_yaml_str(self, node):
+    # Override the default string handling function to return unicode objects always
+    return self.construct_scalar(node)
+yaml.Loader.add_constructor(u'tag:yaml.org,2002:str', construct_yaml_str)
+yaml.SafeLoader.add_constructor(u'tag:yaml.org,2002:str', construct_yaml_str)
+
+
 def parse_args():
     parser = argparse.ArgumentParser(
         description="Standardize your audio encoding and tagging!",
@@ -26,14 +37,19 @@ def parse_args():
     utf8_type = lambda s: unicode(s, "utf8")
     # force these tags (optional)
     override_group = parser.add_argument_group("tag_overrides", "ID3 tag overrides")
-    override_group.add_argument("--album_artist", required=True, type=utf8_type)
-    override_group.add_argument("--album", required=True, type=utf8_type)
-    override_group.add_argument("--year", required=True, type=utf8_type)
+    override_group.add_argument("--album_artist", type=utf8_type)
+    override_group.add_argument("--album", type=utf8_type)
+    override_group.add_argument("--year", type=utf8_type)
     override_group.add_argument("--genre_name", type=utf8_type)
     override_group.add_argument("--comment", type=utf8_type)
     override_group.add_argument("--composer", type=utf8_type)
     override_group.add_argument("--original_artist", type=utf8_type)
     override_group.add_argument("--encoded_by", type=utf8_type)
+
+    parser.add_argument(
+        "--output_dir",
+        default=".",
+    )
 
     parser.add_argument(
         "audio_dirs",
@@ -42,11 +58,13 @@ def parse_args():
         help="Directories of audio to process non-recursively. Multiple directories will be interpreted as multiple discs and will be tagged CD=1,2,3,...",
     )
 
-    return parser.parse_args()
+    args = parser.parse_args()
+    assert os.path.exists(args.output_dir)
+    return args
 
 
 class PendingDisc(object):
-    def __init__(self, pending_audio_files):
+    def __init__(self, pending_audio_files, output_dir):
         needs_conversion = [
             paf.needs_conversion()
             for paf in pending_audio_files
@@ -54,7 +72,15 @@ class PendingDisc(object):
         if any(needs_conversion):
             assert all(needs_conversion)
 
+        dirname = {
+            os.path.dirname(paf.current_filename)
+            for paf in pending_audio_files
+        }
+        assert len(dirname) == 1
+
+        self.dirname = dirname.pop()
         self.pending_audio_files = pending_audio_files
+        self.output_dir = output_dir
 
     def process_disc(self):
         # copy to mp3_tmpdir, reencoding if necessary
@@ -94,10 +120,23 @@ class PendingDisc(object):
             # create output directory and rename
             dirs = set(paf.new_directory for paf in self.pending_audio_files)
             assert len(dirs) == 1
-            os.makedirs(dirs.pop())
+
+            output_dir = dirs.pop()
+            try:
+                os.makedirs(output_dir)
+            except OSError as exc:  # Python >2.5
+                if exc.errno == errno.EEXIST and os.path.isdir(output_dir):
+                    pass
+                else:
+                    raise
 
             for paf in self.pending_audio_files:
                 paf.rename()
+
+            processed_dir = os.path.join(self.output_dir, PROCESSED_DIR)
+            if not os.path.exists(processed_dir):
+                os.mkdir(processed_dir)
+            shutil.move(self.dirname, processed_dir)
 
 
 class PendingAudioFile(object):
@@ -169,11 +208,23 @@ class PendingAudioFile(object):
     def write_tags(self):
         assert self._encoded_fn
         assert self._encoded_fn.endswith(".mp3")
+
+        new_tags = self.proposed_tags
+        assert new_tags.album_artist
+        assert new_tags.year
+        assert new_tags.album
+        assert new_tags.title
+        assert new_tags.cd_no
+        assert new_tags.track_no
+        assert self.num_total_discs
+        assert new_tags.cd_tracks
+
         return audioformat.mp3.write_tags(self._encoded_fn, self.proposed_tags)
 
     def rename(self):
         assert self._encoded_fn
         assert os.path.exists(self._encoded_fn)
+        assert not os.path.exists(self.new_filename)
         shutil.move(self._encoded_fn, self.new_filename)
 
     def __repr__(self):
@@ -185,14 +236,15 @@ def get_tag_overrides(args):
     overrides = {
         k: v
         for k, v in vars(args).iteritems()
-        if k not in ("audio_dirs") and v is not None
+        if k not in ("audio_dirs", "output_dir") and v is not None
     }
-    if overrides["album_artist"] != "Various Artists":
+    if ("album_artist" in overrides
+            and overrides["album_artist"] != "Various Artists"):
         overrides["artist"] = overrides["album_artist"]
     return overrides
 
 
-def get_pending_discs(audio_dirs, global_tag_overrides):
+def get_pending_discs(audio_dirs, global_tag_overrides, output_dir):
     pending_discs = []
     for disc_num, d in enumerate(audio_dirs, 1):
         audio_files = collector.collect_audio_files(d)
@@ -212,12 +264,45 @@ def get_pending_discs(audio_dirs, global_tag_overrides):
                     overrides,
                     len(audio_dirs),
             ))
-        pending_discs.append(PendingDisc(pending_audio_files=pending_audio_files))
+        pending_discs.append(PendingDisc(pending_audio_files, output_dir))
 
     return pending_discs
 
 
-def update_pending_discs(initial_pending_discs):
+def print_pending_discs(pending_discs):
+    def get_update_str(new_val, old_val):
+        if new_val == old_val:
+            return old_val
+        return "{} ({})".format(
+            pycolor.color_string(unicode(new_val), attribute="bold", fg_color="green"),
+            old_val,
+        )
+
+    def print_2_column_table(cells):
+        table = list(itertools.izip_longest(cells[0:(len(cells)/2)], cells[(len(cells)/2):], fillvalue=""))
+        print tabulate.tabulate(table, tablefmt="plain")
+
+    for disc in pending_discs:
+        for af in disc.pending_audio_files:
+            print get_update_str(af.new_filename, af.current_filename)
+            cells = []
+            for slot in audioformat.util.Tags.__slots__:
+                cells.append("{}: {}".format(
+                    slot,
+                    get_update_str(getattr(af.proposed_tags, slot), getattr(af.current_tags, slot)),
+                ))
+            print_2_column_table(cells)
+
+
+def update_pending_discs(initial_pending_discs, output_dir):
+    print_pending_discs(initial_pending_discs)
+
+    print
+    print "Ready to edit? Skip by entering 'n'. Ctrl+C to exit."
+    maybe_no = raw_input()
+    if maybe_no == 'n':
+        return initial_pending_discs
+
     paf_by_filename = {}
     with tempfile.NamedTemporaryFile() as tmpfile:
         display_yaml = []
@@ -241,7 +326,7 @@ def update_pending_discs(initial_pending_discs):
         subprocess.call([editor, tmpfile.name])
 
         tmpfile.seek(0)
-        loaded_yaml = yaml.load(tmpfile)
+        loaded_yaml = yaml.safe_load(tmpfile)
 
     pending_discs = []
     for disc in loaded_yaml:
@@ -256,32 +341,11 @@ def update_pending_discs(initial_pending_discs):
                 ),
             )
         pending_discs.append(
-            PendingDisc(pending_audio_files),
+            PendingDisc(pending_audio_files, output_dir),
         )
     assert len(paf_by_filename) == 0, "Expected to find all filenames!"
 
-    def get_update_str(new_val, old_val):
-        if new_val == old_val:
-            return old_val
-        return "{} ({})".format(
-            pycolor.color_string(unicode(new_val), attribute="bold", fg_color="green"),
-            old_val,
-        )
-
-    def print_2_column_table(cells):
-        table = list(itertools.izip_longest(cells[0:(len(cells)/2)], cells[(len(cells)/2):], fillvalue=""))
-        print tabulate.tabulate(table, tablefmt="plain")
-
-    for disc in pending_discs:
-        for af in pending_audio_files:
-            print get_update_str(af.new_filename, af.current_filename)
-            cells = []
-            for slot in audioformat.util.Tags.__slots__:
-                cells.append("{}: {}".format(
-                    slot,
-                    get_update_str(getattr(af.proposed_tags, slot), getattr(af.current_tags, slot)),
-                ))
-            print_2_column_table(cells)
+    print_pending_discs(pending_discs)
 
     print
     print "Look good? Press Enter. Ctrl+C to exit."
@@ -293,8 +357,8 @@ def update_pending_discs(initial_pending_discs):
 def main():
     args = parse_args()
 
-    initial_pending_discs = get_pending_discs(args.audio_dirs, get_tag_overrides(args))
-    pending_discs = update_pending_discs(initial_pending_discs)
+    initial_pending_discs = get_pending_discs(args.audio_dirs, get_tag_overrides(args), args.output_dir)
+    pending_discs = update_pending_discs(initial_pending_discs, args.output_dir)
 
     for disc in pending_discs:
         disc.process_disc()
